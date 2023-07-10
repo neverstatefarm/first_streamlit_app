b5c09347727fe2829edcab14207c689219cea8e7
neverstatefarm <138711746+neverstatefarm@users.noreply.github.com>
2023-07-10 11:54:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit choice")
 
 
 # take the json version of the repsonse and normalize it
