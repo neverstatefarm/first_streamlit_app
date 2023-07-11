5ef3ae4874fa45ebf5f3d5c3f3d60e70c6de0f7c
neverstatefarm <138711746+neverstatefarm@users.noreply.github.com>
2023-07-11 11:25:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
 streamlit.header('Fruityvice Fruit Advice!')
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
-streamlit.error('Please select a fruit to get information.')
+streamlit.error("Please select a fruit to get information.")
 import requests
 else :
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'fruit choice')
